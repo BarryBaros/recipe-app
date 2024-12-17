ac9b9c1274aab18261cb83d4e60c649514c8ea0e
Barasa <timbaros87@gmail.com>
2024-12-17 23:53:33 +0300

    Create Search component

---
+++ src/components/Search.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "46934b18cf4846678ab5dbe368c1aba7";
+
+export default function Search() {
+  const [query, setQuery] = useState("pizza");
+  //   Use Effect Hook syntax
+  useEffect(() => {
+   async function fetchFood() {
+      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data);
+    }
+    fetchFood();
+  }, []);
+  return (
+    <div>
+      <input
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        type="text"
+      />
+    </div>
+  );
+}
