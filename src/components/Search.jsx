5eb2c802561a62113d3f34222f13cd1372d95d8f
Barasa <timbaros87@gmail.com>
2025-01-01 17:10:24 +0300

    Create FoodList & FoodItem Components

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,17 +3,18 @@ import { useEffect, useState } from "react";
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
 const API_KEY = "46934b18cf4846678ab5dbe368c1aba7";
 
-export default function Search() {
+export default function Search({ foodData, setFoodData }) {
   const [query, setQuery] = useState("pizza");
   //   Use Effect Hook syntax
   useEffect(() => {
-   async function fetchFood() {
+    async function fetchFood() {
       const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
       const data = await res.json();
-      console.log(data);
+      console.log(data.results);
+      setFoodData(data.results);
     }
     fetchFood();
-  }, []);
+  }, [query]);
   return (
     <div>
       <input
