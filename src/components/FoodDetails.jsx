a7270bb409a0f4f6aa3547d6d3b3ee7e7b71767b
Barasa <timbaros87@gmail.com>
2025-01-20 13:40:20 +0300

    Fetch Instructions

--- src/components/FoodDetails.jsx
+++ src/components/FoodDetails.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 
 export default function FoodDetails({ foodId }) {
-  const [food, setFood]=useState({})
+  const [food, setFood] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
   const URL = `https://api.spoonacular.com/recipes/${foodId}/information`;
   const API_KEY = "46934b18cf4846678ab5dbe368c1aba7";
   useEffect(() => {
@@ -9,12 +10,40 @@ export default function FoodDetails({ foodId }) {
       const res = await fetch(`${URL}?apiKey=${API_KEY}`);
       const data = await res.json();
       console.log(data);
-      setFood(data)
+      setFood(data);
+      setIsLoading(false);
     }
     fetchFood();
   }, [foodId]);
-  return <div>Food Details {foodId}
-  {food.title}
-  <img src={food.image} alt="" />
-  </div>;
+  return (
+    <div>
+      <div>
+        <h1> {food.title}</h1>
+        <img src={food.image} alt="" />
+        <div>
+          <span>
+            <strong>🕒{food.readyInMinutes} Minutes</strong>
+          </span>
+          <span>
+            🧑‍🤝‍🧑<strong>Serves {food.servings}</strong>
+          </span>
+          <span>{food.vegetarian ? "🥕Vegetarian" : "🍖Non-Vegetarian"}</span>
+          <span>{food.vegan}? "🐄Vegan":""</span>
+        </div>
+        <div>
+          <span>${food.pricePerServing / 100} Per serving</span>
+        </div>
+      </div>
+
+      <div>
+        <h2>Instructions</h2>
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          food.analyzedInstructions[0].steps.map((step) => <li>{step.step}</li>)
+        )}
+        
+      </div>
+    </div>
+  );
 }
